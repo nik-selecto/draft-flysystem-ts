c0183618ad4b402d9e38a306ada27eb04bff090a
nik <n.moiseienko@selectoglobal.com>
2022-05-13 11:44:19 +0300

    .filesGetTemporaryLink()

--- packages/drop-box-adapter/src/drop-box.adapter.ts
+++ packages/drop-box-adapter/src/drop-box.adapter.ts
@@ -24,19 +24,8 @@ export class DropboxAdapter implements IFilesystemAdapter {
         throw new Error('This method is not implemented yet');
 
     }
-    async readStream(url: string, config?: {
-        /**
-        * If the shared link is to a folder, this parameter can be used to
-        * retrieve the metadata for a specific file or sub-folder in this folder.
-        * A relative path should be used.
-        */
-        path?: string;
-        /**
-         * If the shared link has a password, this parameter can be used.
-         */
-        link_password?: string;
-    } | undefined): Promise<ReadStream> {
-        const res = await this.dbx.sharingGetSharedLinkFile({ url, ...config });
+    async readStream(path: string, config?: Record<string, any> | undefined): Promise<ReadStream> {
+        const res = await this.dbx.filesGetTemporaryLink({ path });
 
         console.log(res);
 

0f6ef1026ee5494ffec7f3ef052597f73079a5f0
nik-selecto <n.moiseienko@selectoglobal.com>
2022-06-02 18:19:32 +0300

    drop-box tests

--- packages/drop-box-adapter/__tests__/drop-box-adapter.test.ts
+++ packages/drop-box-adapter/__tests__/drop-box-adapter.test.ts
@@ -1,7 +1,32 @@
-const dropBoxAdapter = require('../src');
+import fs from 'fs';
+import { join } from 'path';
+import readline from 'readline';
+import { Filesystem } from '@draft-flysystem-ts/flysystem';
+import { inspect } from 'util';
+import { DropboxAuth } from 'dropbox';
+import { DropboxAdapter } from '../src';
 
-describe('drop-box-adapter', () => {
-    it('needs tests', () => {
-        expect(dropBoxAdapter).toBeDefined();
+describe('DropbBox adapter testing', () => {
+    let flysystem!: Filesystem<DropboxAdapter>;
+
+    beforeAll(async () => {
+        flysystem = new Filesystem(new DropboxAdapter({
+            auth: new DropboxAuth({
+                accessToken: process.env.DBX_ACCESS,
+                refreshToken: process.env.DBX_REFRESH,
+            }),
+        }));
     });
+
+    it('Should not have errors', async () => {
+        try {
+            const res = await flysystem.listContents();
+
+            console.log(inspect(res, { colors: true, depth: null }));
+        } catch (error) {
+            console.error(inspect(error, { colors: true, depth: null }));
+
+            expect(error).not.toBeDefined();
+        }
+    }, 10 * 1000);
 });
